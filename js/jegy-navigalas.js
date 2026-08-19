c4184e0b7782d273996ea29fb542f5e54e8fc28a
Zsolt <zsoltbenedek98@gmail.com>
2026-08-19 17:15:06 +0200

    bekerűlt a szeptember előtti augusztus 20 as program amire a 404 et dobja be hisz már nem lehet foglalni oda jegyet.

--- js/jegy-navigalas.js
+++ js/jegy-navigalas.js
@@ -2,8 +2,15 @@ const ticketButtons = document.querySelectorAll(".jegy-gomb, .ticket");
 
 ticketButtons.forEach(button => {
     button.addEventListener("click", event => {
-        event.preventDefault();
         const id = button.dataset.id;
-        window.location.href = `sites/jegyvasarlas.html?id=${id}`;
+
+        // Csak akkor avatkozunk be JS-sel, ha van megadva data-id
+        if (id) {
+            event.preventDefault();
+            window.location.href = `sites/jegyvasarlas.html?id=${id}`;
+        }
+        // Ha nincs data-id (pl. a 404.html gombnál), 
+        // nem hívunk event.preventDefault()-ot, 
+        // így a böngésző a HTML-ben lévő href="404.html" hivatkozást fogja követni.
     });
 });
