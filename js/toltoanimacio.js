31d6cc229d4fff74778cab1cefa82d126f309a1a
Szabó Zoltán <szabozoltan0108@gmail.com>
2026-08-25 20:19:34 +0200

    Töltő képernyő csak első megnyitásra és új ablakban vagy lapon aktiválódik és nem lehet görgetni közben.

--- js/toltoanimacio.js
+++ js/toltoanimacio.js
@@ -1,59 +1,73 @@
-const festekfent = document.querySelector(".logosavfelso");
-const festeklent = document.querySelector(".logosavalso");
-const felirat = document.querySelector(".logoszoveg");
-const tolto = document.querySelector(".tolto");
-const toltosav = document.querySelector(".toltosav");
-const toltofelirat = document.getElementById("szazalek");
 const loaderContent = document.querySelector(".loader-animation");
-let toltesertek = 0;
-let szazalek = 0;
 
-document.body.style.overflow = "hidden";
-felirat.classList.add("feliratjon");
-festekfent.classList.add("festekjon");
-festeklent.classList.add("festekjon");
+if (sessionStorage.getItem("loaderShown")) {
 
-setTimeout(() => {
-    felirat.classList.remove("feliratjon");
-    festekfent.classList.remove("festekjon");
-    festeklent.classList.remove("festekjon");
+    loaderContent.style.display = "none";
 
-    tolto.style.opacity = "1";
+} else {
 
-    felirat.classList.add("beat");
-    festekfent.classList.add("lelegzik1");
-    festeklent.classList.add("lelegzik2");
-    toltes();
-}, 1500);
+    sessionStorage.setItem("loaderShown", "true");
 
-function toltes (){
-    toltesInterval = setInterval(() => {
-        if (toltesertek < 240) {
-            toltesertek = toltesertek + 2.4;
-            toltosav.style.width = toltesertek + "px";
-            szazalek = szazalek + 1;
-            toltofelirat.innerText = `${szazalek}%`;
-        } else {
-            clearInterval(toltesInterval);
-            kesz();
-        }
-    }, 30)
-}
+    const festekfent = document.querySelector(".logosavfelso");
+    const festeklent = document.querySelector(".logosavalso");
+    const felirat = document.querySelector(".logoszoveg");
+    const tolto = document.querySelector(".tolto");
+    const toltosav = document.querySelector(".toltosav");
+    const toltofelirat = document.getElementById("szazalek");
+    const loaderContent = document.querySelector(".loader-animation");
+    let toltesinterval;
+    let toltesertek = 0;
+    let szazalek = 0;
 
-function kesz (){
-    felirat.classList.remove("beat");
-    festekfent.classList.remove("lelegzik1");
-    festeklent.classList.remove("lelegzik2");
-
-    tolto.style.opacity = "0";
-
-    felirat.classList.add("animout");
-    festekfent.classList.add("animout");
-    festeklent.classList.add("animout");
-    loaderContent.style.opacity = "0";
+    document.documentElement.style.overflow = "hidden";
+    document.body.style.overflow = "hidden";
+    felirat.classList.add("feliratjon");
+    festekfent.classList.add("festekjon");
+    festeklent.classList.add("festekjon");
 
     setTimeout(() => {
-        loaderContent.style.display = "none";
-        document.body.style.overflow = "";
-    }, 1000)
-};
+        felirat.classList.remove("feliratjon");
+        festekfent.classList.remove("festekjon");
+        festeklent.classList.remove("festekjon");
+
+        tolto.style.opacity = "1";
+
+        felirat.classList.add("beat");
+        festekfent.classList.add("lelegzik1");
+        festeklent.classList.add("lelegzik2");
+        toltes();
+    }, 1500);
+
+    function toltes (){
+        toltesInterval = setInterval(() => {
+            if (toltesertek < 240) {
+                toltesertek = toltesertek + 2.4;
+                toltosav.style.width = toltesertek + "px";
+                szazalek = szazalek + 1;
+                toltofelirat.innerText = `${szazalek}%`;
+            } else {
+                clearInterval(toltesInterval);
+                kesz();
+            }
+        }, 11)
+    }
+
+    function kesz (){
+        felirat.classList.remove("beat");
+        festekfent.classList.remove("lelegzik1");
+        festeklent.classList.remove("lelegzik2");
+
+        tolto.style.opacity = "0";
+
+        felirat.classList.add("animout");
+        festekfent.classList.add("animout");
+        festeklent.classList.add("animout");
+        loaderContent.style.opacity = "0";
+
+        setTimeout(() => {
+            loaderContent.style.display = "none";
+            document.documentElement.style.overflow = "";
+            document.body.style.overflow = "";
+        }, 1000)
+    };
+}
