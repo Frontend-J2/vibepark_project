94901381e2bfd41ade501a644de793a187f46e3e
Szabó Zoltán <szabozoltan0108@gmail.com>
2026-09-01 11:29:01 +0200

    Világos mód Local storage és gomb módosítás. Fotter minden oldalra.

--- js/vilagosmod.js
+++ js/vilagosmod.js
@@ -1,7 +1,29 @@
+const light = localStorage.getItem("lightMode");
+const lightBtn = document.querySelector(".theme-in");
+lightModeInner();
+
+if (light === "true"){
+    document.body.classList.add("light-mode");
+    lightBtn.classList.add("on");
+    lightModeInner();
+}
+
 const themeBtn = document.querySelector("#themeBtn")
-
-
 
 themeBtn.addEventListener("click", function(){
     document.body.classList.toggle("light-mode");
+    lightBtn.classList.toggle("on");
+
+    const light = document.body.classList.contains("light-mode");
+    localStorage.setItem("lightMode", light);
+    lightModeInner();
 });
+
+function lightModeInner(){
+    const light = localStorage.getItem("lightMode");
+    if (light === "true"){
+        lightBtn.innerText = "☀️"
+    } else {
+        lightBtn.innerText = "🌙"
+    }
+}
